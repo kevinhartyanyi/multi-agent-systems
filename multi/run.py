79f708275625b2353bf13c720e162995797c3e47
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2020-12-03 20:13:57 +0100

    Fix communication

--- multi/run.py
+++ multi/run.py
@@ -4,9 +4,9 @@ from multiagentenv import MultiAgentEnv
 #https://github.com/cyoon1729/Multi-agent-reinforcement-learning/tree/master/MADDPG
 
 if __name__ == "__main__":
-    num_agents = 3
+    num_agents = 10
     num_episodes = 11
-    num_steps = 15
+    num_steps = 500
 
     env = MultiAgentEnv(num_agents)
     ma_controller = MADDPG(env, 1000000)
